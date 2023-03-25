69bf151ccfadde9577e20fc2b759b593ba6ab262
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-03-25 18:35:17 +0000

    Add .herokuapp.com to ALLOWED_HOSTS

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -3,6 +3,8 @@ import dj_database_url
 
 from .base import *
 
+ALLOWED_HOSTS = [".herokuapp.com"]
+
 DATABASES["default"] = dj_database_url.config()
 
 STATIC_ROOT = path.join(PROJECT_DIR, "staticfiles")
