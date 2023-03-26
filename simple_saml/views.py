003c8f9c4b4a2594afc80616b6c1944f54e34f12
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-03-26 21:56:00 +0100

    Add decorator to SAML XML view

--- simple_saml/views.py
+++ simple_saml/views.py
@@ -1,17 +1,15 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from social_django.utils import load_backend, load_strategy
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def saml_metadata_view(request: HttpRequest) -> HttpResponse:
-    """View to generate the SAML metadata XML."""
-    saml_backend = load_backend(
-        load_strategy(request),
-        "saml",
-        redirect_uri=reverse("social:complete", args=("saml",)),
-    )
+    """View to generate the SAML metadata XML (superuser only)."""
+    uri = (reverse("social:complete", args=("saml",)),)
+    saml_backend = load_backend(load_strategy(request), "saml", redirect_uri=uri)
     metadata, errors = saml_backend.generate_metadata_xml()
     if not errors:
         return HttpResponse(content=metadata, content_type="text/xml")
-
     raise Exception("Error loading SAML backend.")
