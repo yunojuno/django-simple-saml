a8f2947d5e482f5dae1370ad32b09d0a8a4bca4c
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-03-27 15:43:53 +0100

    Fix social_auth bug in template

--- demo/views.py
+++ demo/views.py
@@ -5,12 +5,12 @@ from simple_saml.models import IdentityProvider
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    if request.user.is_authenticated and not request.user.is_staff:
-        auth = request.user.social_auth.get()
-    else:
-        auth = None
+    # if request.user.is_authenticated and not request.user.is_staff:
+    #     auth = request.user.social_auth.get()
+    # else:
+    #     auth = None
     return render(
         request,
         "index.html",
-        {"identity_providers": IdentityProvider.objects.active(), "auth": auth},
+        {"identity_providers": IdentityProvider.objects.active(), "auth": None},
     )
