f8e27cea23b7c1d2ffbafc4068899a547d586e28
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-03-28 10:44:28 +0100

    Update index template

--- demo/views.py
+++ demo/views.py
@@ -5,12 +5,8 @@ from simple_saml.models import IdentityProvider
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    # if request.user.is_authenticated and not request.user.is_staff:
-    #     auth = request.user.social_auth.get()
-    # else:
-    #     auth = None
     return render(
         request,
         "index.html",
-        {"identity_providers": IdentityProvider.objects.active(), "auth": None},
+        {"identity_providers": IdentityProvider.objects.active()},
     )
